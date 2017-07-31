c55396d74fd8d71cb47fccf37ce3955cb3a6ac88
Julian Feil <j.feil@ostfalia.de>
2017-07-31 19:11:06 +0200

    Socket in Threads

--- app/src/main/java/Server_Connection_Handler/Server_Connection_Handler.java
+++ app/src/main/java/Server_Connection_Handler/Server_Connection_Handler.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class Server_Connection_Handler implements Server_Connection_Handler_Interface
 {
@@ -24,59 +25,61 @@ public class Server_Connection_Handler implements Server_Connection_Handler_Inte
 
     private List<List<String>> GetInfosfromServer(int infoindex, int restaurant_id) throws UnknownHostException, IOException
     {
+    	List<List<String>> infoarray = new ArrayList<List<String>>();
+    	CountDownLatch latch = new CountDownLatch(1);
+    	
         new Thread(new Runnable(){
             public void run(){
-                //open socket
+            	Socket socket = new Socket("77.20.227.221", portNumber);
+            	
+
+                //infoindex als output an den Server gibt an, welche Infos angefordert werden
+                //Siehe Namen der unten stehenden Funktionen und die and diese Funktion uebergebenen Werte oder die Dokumentation als Referenz
+                OutputStream out = socket.getOutputStream();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                if(infoindex == 1 || infoindex == 2)
+                {
+                    out.write(infoindex);
+                    out.write(restaurant_id);
+                }
+                else
+                {
+                    out.write(infoindex);
+                }
+                out.flush();
+                out.close();
+
+                for(int i = 0; i < 0; i++)
+                {
+                    List<String> templist = new ArrayList<String>();
+                    for(int i2 = 0; i2 < 0; i2++)
+                    {
+                        //Wenn temp == "", neue Liste.
+                        String temp = br.readLine();
+                        if(temp == "")
+                        {
+                            i = -2;
+                            break;
+                        }
+
+                        //Wenn temp == null, br leer.
+                        if(temp == null)
+                        {
+                            break;
+                        }
+
+                        templist.add(temp);
+                    }
+                    infoarray.add(templist);
+                }
+                latch.countDown();
+                socket.close();
             }
         }).start();
-        List<List<String>> infoarray = new ArrayList<List<String>>();
-
-//        InetAddress serverAddr = InetAddress.getByName(host);
-        Socket socket = new Socket("77.20.227.221", portNumber);
-
-        //infoindex als output an den Server gibt an, welche Infos angefordert werden
-        //Siehe Namen der unten stehenden Funktionen und die and diese Funktion uebergebenen Werte oder die Dokumentation als Referenz
-        OutputStream out = socket.getOutputStream();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        if(infoindex == 1 || infoindex == 2)
-        {
-            out.write(infoindex);
-            out.write(restaurant_id);
-        }
-        else
-        {
-            out.write(infoindex);
-        }
-        out.flush();
-        out.close();
-
-        for(int i = 0; i < 0; i++)
-        {
-            List<String> templist = new ArrayList<String>();
-            for(int i2 = 0; i2 < 0; i2++)
-            {
-                //Wenn temp == "", neue Liste.
-                String temp = br.readLine();
-                if(temp == "")
-                {
-                    i = -2;
-                    break;
-                }
-
-                //Wenn temp == null, br leer.
-                if(temp == null)
-                {
-                    break;
-                }
-
-                templist.add(temp);
-            }
-            infoarray.add(templist);
-        }
-
-        socket.close();
+        
+        latch.await();
         return infoarray;
     }
 
@@ -97,23 +100,34 @@ public class Server_Connection_Handler implements Server_Connection_Handler_Inte
 
     public void OrderwithCredits(List<List<String>> orderarray) throws IOException
     {
-        Socket socket = new Socket(host, portNumber);
+    	CountDownLatch latch = new CountDownLatch(1);
+    	
+        new Thread(new Runnable(){
+            public void run(){
+            	Socket socket = new Socket(host, portNumber);
 
-        OutputStream out = socket.getOutputStream();
+            	OutputStream out = socket.getOutputStream();
 
-        PrintWriter pr = new PrintWriter(out);
+            	PrintWriter pr = new PrintWriter(out);
 
-        for(int i = 0; i < orderarray.size(); i++)
-        {
-            for(int i2 = 0; i2 < orderarray.get(i).size(); i2++)
-            {
-                pr.println(orderarray.get(i).get(i2));
-            }
-            pr.println("");
-            //pr.flush(); Achtung, moegliche Fehlerquelle.
-        }
+            	for(int i = 0; i < orderarray.size(); i++)
+            	{
+            		for(int i2 = 0; i2 < orderarray.get(i).size(); i2++)
+            		{
+            			pr.println(orderarray.get(i).get(i2));
+            		}
+            		pr.println("");
+            		//pr.flush(); Achtung, moegliche Fehlerquelle.
+            	}
 
-        out.flush();
-        socket.close();
+            	out.flush();
+            	latch.countDown();
+            	socket.close();
+        
+            }).start();
+        
+
+//      InetAddress serverAddr = InetAddress.getByName(host);
+      latch.await();
     }
 }
